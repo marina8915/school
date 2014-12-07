2d0cce3577582ae76efbbb6d45bc40f87b9becb8
marina <marina_bakum@mail.ru>
2014-12-07 16:32:19 +0200

    console comand "Hello world"

---
+++ console/controllers/ShowController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace console\controllers;
+
+use yii\console\Controller;
+use yii\helpers\Console;
+
+class ShowController extends Controller
+{
+    public function actionIndex()
+    {
+     // Добавляєм запис в файл
+     file_put_contents('console/hello.txt', "Hello World!\n", FILE_APPEND | LOCK_EX);
+     //Виведення повідомленняв консоль
+     $this->stdout('Done!' . PHP_EOL, Console::FG_GREEN);
+    }
+       
+}
+
